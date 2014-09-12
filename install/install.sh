ed81a7ad1dafdf1c2e7cae06be5ccbfadacb2703
Douglas Squirrel <ds@douglassquirrel.com>
2014-09-13 00:06:46 +0100

    install psql

--- install/install.sh
+++ install/install.sh
@@ -2,6 +2,7 @@ sudo apt-get -y install rabbitmq-server
 sudo apt-get -y install python-dev
 sudo apt-get -y install python-pip
 sudo apt-get -y install libpq-dev
+sudo apt-get -y install postgresql-client
 
 sudo pip install pika
 sudo pip install psycopg2
