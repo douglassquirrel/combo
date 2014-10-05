f057c1d653fe83e9f0fcb2908ae459f897bd6427
Douglas Squirrel <ds@douglassquirrel.com>
2014-10-05 12:07:22 +0100

    basic topics handling

--- tests/unit/combo_server_test.py
+++ tests/unit/combo_server_test.py
@@ -1,14 +1,21 @@
 #! /usr/bin/env python  
 
-from web import combo_server
+from json import loads
 from unittest import TestCase
+from web import combo_server
 
 TEST_HOME_PAGE = 'test home page'
+TEST_TOPICS = ['headlines', 'story.creation', 'story.update']
+
+class MockFactspace:
+    def list_topics(self):
+        return TEST_TOPICS
 
 class ComboServerTest(TestCase):
     def setUp(self):
         self.app = combo_server.app
         self.app.testing = True
+        self.app.config['FACTSPACE'] = MockFactspace()
         self.client = self.app.test_client()
 
     def tearDown(self):
@@ -20,3 +27,9 @@ class ComboServerTest(TestCase):
         self.assertEqual(200, response.status_code)
         self.assertEqual('text/html; charset=utf-8', response.content_type)
         self.assertEqual(TEST_HOME_PAGE, response.data)
+
+    def test_topics(self):
+        response = self.client.get('/topics')
+        self.assertEqual(200, response.status_code)
+        self.assertEqual('application/json', response.content_type)
+        self.assertEqual(TEST_TOPICS, loads(response.data))
--- web/combo_server.py
+++ web/combo_server.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python
 
 from flask import Flask, Response
+from json import dumps
 from os import environ
 from os.path import dirname, join as pathjoin
 
@@ -10,10 +11,18 @@ app = Flask('combo')
 def home():
     return respond(app.config['HOME_HTML'], mimetype='text/html')
 
+@app.route('/topics')
+def topics():
+    topics = app.config['FACTSPACE'].list_topics()
+    return respond_json(topics)
+
 def respond(data, mimetype):
     response = Response(data, mimetype=mimetype)
     response.charset = app.config['CHARSET']
     return response
+
+def respond_json(data):
+    return respond(dumps(data), mimetype='application/json')
 
 default_config_file = pathjoin(dirname(__file__), 'settings.cfg')
 config_file = environ.get('COMBO_SETTINGS_FILE', default_config_file)
