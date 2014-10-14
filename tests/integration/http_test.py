c8936f51b451af729276f548a14978e54d622781
Douglas Squirrel <ds@douglassquirrel.com>
2014-10-14 21:59:51 +0100

    publish and release integration test

--- tests/integration/http_test.py
+++ tests/integration/http_test.py
@@ -1,8 +1,9 @@
 #! /usr/bin/env python
 
 from httplib import HTTPConnection
-from json import dumps
+from json import dumps, loads
 from sys import argv
+from time import sleep
 from unittest import main, TestCase
 from uuid import uuid1
 
@@ -27,10 +28,34 @@ class HTTPTest(TestCase):
         response = self.conn.getresponse()
         self._check_response(response, 202, 'text/plain; charset=utf-8')
 
+    def test_publish_and_retrieve(self):
+        topic = self._new_unique_topic()
+        self.conn.request('POST', 'topics/%s/facts' % (topic,),
+                          dumps(FACTS[0]))
+        response = self.conn.getresponse()
+        self.assertEqual(202, response.status)
+        response.read()
+        sleep(0.1)
+        self.conn.request('GET', 'topics/%s/facts' % (topic,))
+        response = self.conn.getresponse()
+        self._check_response(response, 200, 'application/json')
+        returned_facts = loads(response.read())
+        self.assertEqual(1, len(returned_facts))
+        raw_fact = self._check_and_extract(returned_facts[0])
+        self.assertEqual(FACTS[0], raw_fact)
+
     def _check_response(self, resp, expected_status, expected_content_type):
         self.assertEqual(expected_status, resp.status)
         actual_content_type = resp.getheader('Content-Type')
         self.assertEqual(expected_content_type, actual_content_type)        
+
+    def _check_and_extract(self, returned_fact):
+        self.assertIn('combo_id', returned_fact)
+        self.assertIn('combo_timestamp', returned_fact)
+        raw_fact = dict(returned_fact)
+        del raw_fact['combo_id']
+        del raw_fact['combo_timestamp']
+        return raw_fact
 
     def _new_unique_topic(self):
         return uuid1()
