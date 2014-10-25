1f8e9cf1fd9660ff91e380fd6c39b2b8226dfdfb
Douglas Squirrel <ds@douglassquirrel.com>
2014-10-25 12:15:01 +0100

    close connections

--- web/factspace.py
+++ web/factspace.py
@@ -26,30 +26,39 @@ class Factspace:
         result = run_sql(conn, CHECK_FACTS_TABLE_SQL, results=True)[0][0]
         if result != 1:
             raise MissingTableError('Facts table not present.')
+        conn.close()
 
     def list_topics(self):
         conn = connect(self.url)
         result = run_sql(conn, GET_TOPICS_SQL, results=True)
-        return map(lambda x: x[0], result)
+        ret_val = map(lambda x: x[0], result)
+        conn.close()
+        return ret_val
 
     def last_n(self, topic, number):
         conn = connect(self.url)
         topic = self._translate_wildcards(topic)
         result = run_sql(conn, GET_LAST_N_FACTS_SQL % (number,),
                          results=True, parameters=[topic])
-        return map(self._format_fact, result)
+        ret_val = map(self._format_fact, result)
+        conn.close()
+        return ret_val
 
     def after_id(self, topic, id):
         conn = connect(self.url)
         topic = self._translate_wildcards(topic)
         result = run_sql(conn, GET_AFTER_ID_FACTS_SQL,
                          results=True, parameters=[topic, id])
-        return map(self._format_fact, result)
+        ret_val = map(self._format_fact, result)
+        conn.close()
+        return ret_val
 
     def add_fact(self, topic, fact):
         conn = connect(self.url)
         run_sql(conn, INSERT_FACT_SQL, results=False,
                 parameters=[topic, dumps(fact)])
+        conn.close()
+        return
 
     def _format_fact(self, returned_fact):
         fact = dict(returned_fact[3])
