8aeed46eb5207ca3ce2e5606d3838fe66c6f9198
Douglas Squirrel <ds@douglassquirrel.com>
2015-01-22 18:26:40 +0000

    no more haigha

--- install/install.sh
+++ install/install.sh
@@ -4,6 +4,5 @@ sudo apt-get -y install python-pip
 sudo apt-get -y install libpq-dev
 sudo apt-get -y install postgresql-client
 
-sudo pip install haigha
 sudo pip install pika
 sudo pip install psycopg2
